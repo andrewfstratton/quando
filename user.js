ecfc3f7700616a7eaaab3bc5e1f100fda8906dc0
andrewfstratton <A.Stratton@shu.ac.uk>
2017-11-14 15:03:46 +0000

    Refactor - standard Javascript

--- user.js
+++ user.js
@@ -1,31 +1,31 @@
-"use strict"
+'use strict'
 const db = require('./db')
 const COLLECTION = 'user'
 
 exports.getOnIdPassword = (id, password) => {
-    return new Promise((success, fail) => {
-        const query = { _id: { $eq: id }, password: { $eq: password } }
-        const options = { limit: 1 }
-        const fields = { _id: 0, password: 0 }
-        db.getArray(COLLECTION, query, fields, options).then(
+  return new Promise((success, fail) => {
+    const query = { _id: { $eq: id }, password: { $eq: password } }
+    const options = { limit: 1 }
+    const fields = { _id: 0, password: 0 }
+    db.getArray(COLLECTION, query, fields, options).then(
             (result) => {
-                if (result.length == 1) {
-                    let user_doc = result[0]
-                    user_doc.id = id
+              if (result.length == 1) {
+                let user_doc = result[0]
+                user_doc.id = id
                     // decode the current_script
-                    user_doc.script_name = decodeURIComponent(user_doc.script_name)
+                user_doc.script_name = decodeURIComponent(user_doc.script_name)
                     // no need to store deployed name - will be in script
-                    success(user_doc)
-                } else {
-                    fail(Error("Failed to find unique user"))
-                }
+                success(user_doc)
+              } else {
+                fail(Error('Failed to find unique user'))
+              }
             }, fail)
-    })
+  })
 }
 
 exports.save = (userid, password, script_name) => {
-    return new Promise((success, fail) => {
-        let doc = { _id: userid, password: password, script_name: script_name }
-        db.save(COLLECTION, doc).then(success, fail)
-    })
+  return new Promise((success, fail) => {
+    let doc = { _id: userid, password: password, script_name: script_name }
+    db.save(COLLECTION, doc).then(success, fail)
+  })
 }
