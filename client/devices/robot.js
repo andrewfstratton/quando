d851cbf9d09f19da4101339e3462fe33255647d8
Terramet <josh.bamforth@git.com>
2017-12-21 13:19:16 +0000

    merging

--- client/devices/robot.js
+++ client/devices/robot.js
@@ -1,11 +1,30 @@
 (function () {
     var quando = this['quando']
     if (!quando) {
-      alert('Fatal Error: ubit must be included after quando_browser')
+      alert('Fatal Error: Robot must be included after quando_browser')
     }
 
     var self = quando.robot = {}
-    self.say = function(text) {
-        alert(text);
+    self.say = function(text, extras) {
+        session.service("ALTextToSpeech").done(function (tts) {
+            // tts is a proxy to the ALTextToSpeech service
+            if(extras.speed) {
+                tts.setParameter("speed", extras.speed)
+            }
+            tts.say(text);
+            tts.resetSpeed();
+          }).fail(function (error) {
+            console.log("An error occurred:", error);
+          });
+    }
+
+    self.connect = function(robotIp) {
+        session = new QiSession(robotIp);       
+        session.socket().on('connect', function () {
+            console.log('QiSession connected!');
+            // now you can start using your QiSession
+          }).on('disconnect', function () {
+            console.log('QiSession disconnected!');
+          });
     }
 })
