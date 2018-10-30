62909b5b195da78763a5d561b964f7a2a38858d1
andrewfstratton <A.Stratton@shef.ac.uk>
2018-10-30 09:46:55 +0000

    Add Image move

--- client/modules/image.js
+++ client/modules/image.js
@@ -48,4 +48,35 @@
         _rotate(quando.style.DEFAULT, 'Y', val, mid, range, inverted)
     }
 
+    self.in_out = (val, mid, range, inverted) => {
+        // let z = quando.convert_linear(val, mid, range, inverted)
+        // quando.style.set(style, '#quando_image', 'transform', `rotate${axis}(${rad}rad)`);
+        // _rotate(quando.style.DEFAULT, 'Y', val, mid, range, inverted)
+    }
+
+    function _convert_percent_linear(val, mid, range, inverted) {
+        if (val === false) { val = 0.5 }
+        if (inverted) { val = 1 - val }
+        let min = (mid - range)
+        let max = (mid + range)
+        return min + (val * (max-min))
+    }
+
+    function _left_right(style, val, mid, range, inverted) {
+        let x = _convert_percent_linear(val, mid, range, inverted)
+        quando.style.set(style, '#quando_image', 'background-position-x', x + '%');
+    }
+
+    self.left_right = (val, mid, range, inverted) => {
+        _left_right(quando.style.DISPLAY, val, mid, range, inverted)
+    }
+
+    self.left_rightDefault = (val, mid, range, inverted) => {
+        _left_right(quando.style.DEFAULT, val, mid, range, inverted)
+    }
+
+    self.up_down = (val, mid, range, inverted) => {
+        // self.y = _convert_linear(val, mid, range, inverted)
+    }
+
 })()
