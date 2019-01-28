68fe5858dab07cf4f554087a34deb762447d20e1
andrewfstratton <A.Stratton@shef.ac.uk>
2019-01-28 22:20:20 +0000

    Change start to ignore popuchdb on IBM Cloud

--- start.js
+++ start.js
@@ -1,14 +1,17 @@
 // const DB = require('./db')
 const { spawn, spawnSync } = require('child_process')
 
-let pouch_db_process = null
-
-pouch_db_process = spawn('npm', ['run', 'pouchd'], {stdio: 'inherit', shell: true}) // , detached:true})
-pouch_db_process.on('error', (err)=>{
-  console.error('  PouchDB--' + err)
-})
+let appEnv = require('cfenv').getAppEnv() // For IBM Cloud
+if (appEnv.isLocal) { // i.e. not running on cloud server
+  console.log("Starting PouchDB")
+  let pouch_db_process = null
+  pouch_db_process = spawn('npm', ['run', 'pouchd'], {stdio: 'inherit', shell: true}) // , detached:true})
+  pouch_db_process.on('error', (err)=>{
+    console.error('  PouchDB--' + err)
+  })
+  console.log('To open Hub Control Panel - http://127.0.0.1) - or Ctrl-C to exit...')
+}
 
 console.log('Starting Quando server...')
-console.log('To open Hub Control Panel - http://127.0.0.1) - or Ctrl-C to exit...')
 app_process = spawnSync('npm', ['run', 'quando'], {stdio: 'inherit', shell: true})
 console.log('Quando server exited...')
