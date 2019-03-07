f6e57a64fd988b8fbc5d964536589daefdad2652
andrewfstratton <A.Stratton@shef.ac.uk>
2019-03-07 16:39:48 +0000

    Use hand.confidence to reduce errors with leap

--- client/modules/leap.js
+++ client/modules/leap.js
@@ -70,43 +70,45 @@
       if (frame.hands.length >= 1) {
         var hand = frame.hands[0]
         quando.idle_reset()
-        var [x, y, z] = hand.palmPosition
-        z = -z // make z increase as the hand moves away from the visitor
-        if (x != self.last_x) {
-          quando.dispatch_event('leapX', {'detail': x})
-          self.last_x = x
-        }
-        if (y != self.last_y) {
-          quando.dispatch_event('leapY', {'detail': y})
-          self.last_y = y
-        }
-        if (z != self.last_z) {
-          quando.dispatch_event('leapZ', {'detail': z})
-          self.last_z = z
-        }
-        var roll = _radians_to_degrees(-hand.roll())
-        if (roll != self.last_roll) {
-          quando.dispatch_event('leapRoll', {'detail': roll})
-          self.last_roll = roll
-        }
-        var pitch = _radians_to_degrees(hand.pitch())
-        if (pitch != self.last_pitch) {
-          quando.dispatch_event('leapPitch', {'detail': pitch})
-          self.last_pitch = pitch
-        }
-        var yaw = _radians_to_degrees(hand.yaw())
-        if (yaw != self.last_yaw) {
-          quando.dispatch_event('leapYaw', {'detail': yaw})
-          self.last_yaw = yaw
-        }
-        var strength = hand.grabStrength
-        if (strength == 1 && self.last_flat) {
-          quando.dispatch_event('leapHandClosed')
-          self.last_flat = false
-        }
-        if (strength <= 0.9 && !self.last_flat) {
-          quando.dispatch_event('leapHandOpen')
-          self.last_flat = true
+        if (hand.confidence == 1) {
+          var [x, y, z] = hand.palmPosition
+          z = -z // make z increase as the hand moves away from the visitor
+          if (x != self.last_x) {
+            quando.dispatch_event('leapX', {'detail': x})
+            self.last_x = x
+          }
+          if (y != self.last_y) {
+            quando.dispatch_event('leapY', {'detail': y})
+            self.last_y = y
+          }
+          if (z != self.last_z) {
+            quando.dispatch_event('leapZ', {'detail': z})
+            self.last_z = z
+          }
+          var roll = _radians_to_degrees(-hand.roll())
+          if (roll != self.last_roll) {
+            quando.dispatch_event('leapRoll', {'detail': roll})
+            self.last_roll = roll
+          }
+          var pitch = _radians_to_degrees(hand.pitch())
+          if (pitch != self.last_pitch) {
+            quando.dispatch_event('leapPitch', {'detail': pitch})
+            self.last_pitch = pitch
+          }
+          var yaw = _radians_to_degrees(hand.yaw())
+          if (yaw != self.last_yaw) {
+            quando.dispatch_event('leapYaw', {'detail': yaw})
+            self.last_yaw = yaw
+          }
+          var strength = hand.grabStrength
+          if (strength == 1 && self.last_flat) {
+            quando.dispatch_event('leapHandClosed')
+            self.last_flat = false
+          }
+          if (strength <= 0.9 && !self.last_flat) {
+            quando.dispatch_event('leapHandOpen')
+            self.last_flat = true
+          }
         }
       }
     }
