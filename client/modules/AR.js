4ddc49dbb4c28cf5d5fcc5c32dbc44fb9ffc651a
balraj-johal <balraj-johal@users.noreply.github.com>
2019-03-15 15:31:03 +0000

    proto on marker loss block added, AR module tidied

--- client/modules/AR.js
+++ client/modules/AR.js
@@ -7,60 +7,6 @@
     self.width = screen.width
     self.height = screen.height
 
-    self.showGLTFold = function(modelURL, markerChoice, scale=100, above=false) {
-
-      //handle params
-      modelURL = '/client/media/' + encodeURI(modelURL)
-      scale = scale/100; //scale supplied in %
-      if (above) { //is model to be on or above marker?
-        position = '0 1 0'
-      } else {
-        position = '0 0 0'
-      }
-
-      //initialize scene
-      var scene = document.getElementById('scene')
-      if (scene == null) {
-        var scene = document.createElement('a-scene');
-        scene.setAttribute('arjs', 'debugUIEnabled: false;');
-        scene.setAttribute('embedded', '');
-        scene.setAttribute('id', 'scene');
-      }
-
-      //camera element
-      var marker = document.createElement('a-marker-camera'); //a-marker doesn't work 
-      //var marker = document.createElement('a-marker'); //a-marker doesn't work 
-      //var cam = document.createElement('a-camera-static'); //a-marker doesn't work 
-
-      if (markerChoice == 'hiro') {
-        marker.setAttribute('preset', 'hiro');
-        marker.setAttribute('id', 'hiro');
-      } else if (markerChoice == 'scan me') {
-
-      } else { 
-        //only remaining options are the lettered markers
-        marker.setAttribute('preset', 'custom');
-        marker.setAttribute('type', 'pattern');
-        marker.setAttribute('id', markerChoice);
-        //NOTE: below URLs must be hosted online instead of relatively for some dumb reason
-        marker.setAttribute('url', 'https://raw.githubusercontent.com/andrewfstratton/quando/adventureGame2/client/media/letters/'+markerChoice+'.patt');
-      }
-
-      //user chosen model - GLTF 2.0 - uncompressed
-      var model = document.createElement('a-gltf-model');
-      model.setAttribute('gltf-model', 'url('+modelURL+')'); //id model from url
-      model.setAttribute('scale', scale.toString() + ' '+ scale.toString() +' '+ scale.toString());
-      model.setAttribute('position', position);
-      
-      //add to heirarchy
-      scene.appendChild(marker);
-      scene.appendChild(model);
-      //scene.appendChild(cam);
-
-      //add to AR element of doc
-      document.getElementById('quando_AR').append(scene);
-    }
-
     self.showGLTF = function(modelURL, markerID, scale=100, above=false) {
 
       //handle params
@@ -159,16 +105,23 @@
       //TODO -- delete VR button w attribute 'AR-injected'
     }
 
-    //test method adding event listener to marker that fires when the marker's scanned
+    //adds event listener to marker that fires when the marker's scanned, executing the 
+    //blocks in the box
     self.onScan = function(markerID, fn) {
-
       var marker = document.getElementById(markerID)
-      alert('listening for marker: ' + marker.id)
 			marker.addEventListener('markerFound', (e)=>{
-				alert('marker found')
         fn()
       });
-      
     }
+
+    //adds event listener to marker that fires when the marker's scanned, executing the 
+    //blocks in the box
+    self.onLoss = function(markerID, delay, fn) {
+      var marker = document.getElementById(markerID)
+			marker.addEventListener('markerLost', (e)=>{
+        fn()
+      });
+    }
+
 
 }) ()
