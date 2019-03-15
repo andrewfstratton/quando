39cc45bf72f610326a7dd96993befef3edf00e98
balraj-johal <balraj-johal@users.noreply.github.com>
2019-03-15 15:00:53 +0000

    minor tidying up
    renamed and messed with the blocks, deleted redundant marker files etc.

--- client/deployed_js/-.js
+++ client/deployed_js/-.js
@@ -1,12 +1,3 @@
 let exec = () => {
-quando.display(1,()=>{
-quando.title('test')
 quando.AR.showGLTF('tests/jupiter.gltf', 'A', 0.1, true)
-quando.AR.onScan('A', ()=>{
-quando.change_inv('add', 'b')
-quando.init_inv_watch('b', ()=>{
-alert('nice')
-})
-})
-})
 }
