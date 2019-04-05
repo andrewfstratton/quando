809462ccd3ba5a78581efe8609863050fc069195
balraj-johal <balraj-johal@users.noreply.github.com>
2019-04-05 15:08:52 +0100

    more AR redesign

--- client/deployed_js/-.js
+++ client/deployed_js/-.js
@@ -1,5 +1,7 @@
 let exec = () => {
-quando.ar.whenMarker('B', 'flat', (markerID = 'B')=>{
-quando.ar.showVideo(markerID, 'test.mp4', 100, 'flat')
+quando.ar.whenMarker('D', 'wall', (markerID = 'D')=>{
+quando.ar.showImage(markerID, 'tree.jpg', 100, 'flat')
 })
+quando.change_inv('add', 'bfg')
+quando.show_inv()
 }
