a12e25c6beaeca4c1b33b43d36075c53da59ec4d
andrewfstratton <A.Stratton@shef.ac.uk>
2019-04-16 22:48:33 +0100

    Refactor makecode servo

--- device_setup/microbit/makecode/usb_servo.js
+++ device_setup/microbit/makecode/usb_servo.js
@@ -9,33 +9,35 @@ basic.showLeds(`
 //basic.pause(750)
 let icons = [IconNames.Happy, IconNames.Sad]
 
-//serial.writeLine("ubit started...")
+serial.writeLine("ubit started...")
 
 while (true) {
     let txt = serial.readLine()
     let eq = txt.indexOf("=")
     let key = txt.substr(0, eq)
     let val = txt.substr(eq + 1)
-    if (key == 'display') {
-        basic.showString(val)
-    } else if (key == 'icon') {
-        basic.showIcon(icons[parseInt(val)])
-    } else if (key == 'turn') {
-        // N.B. Display includes delay - so don't do....
-        let comma = val.indexOf(',')
-        let servo = parseInt(val.substr(0, comma))
-        let angle = parseInt(val.substr(comma + 1))
-        serial.writeLine("msg=" + angle + ", val=" + val)
-        switch (servo) {
-            case 1: {
-                pins.servoWritePin(AnalogPin.P1, angle)
-                break;
+    switch (key) {
+        case 'display':
+            basic.showString(val)
+            break;
+        case 'icon':
+            basic.showIcon(icons[parseInt(val)])
+            break;
+        case 'T':
+            // N.B. Display includes delay - so don't do....
+            let comma = val.indexOf(',')
+            let angle = parseInt(val.substr(0, comma))
+            let servo = parseInt(val.substr(comma + 1))
+//        serial.writeLine("angle=" + angle + ", servo=" + servo + ", val=" + val)
+            let pin = AnalogPin.P20
+            if (servo == 1) {
+                pin = AnalogPin.P1
+            } else if (servo == 2) {
+                pin = AnalogPin.P2
             }
-            case 2: {
-                pins.servoWritePin(AnalogPin.P2, angle)
-                break;
+            if (pin != AnalogPin.P20) {
+                pins.servoWritePin(pin, angle)
             }
-        }
-        //        basic.pause(50)
+            break;
     }
 }
