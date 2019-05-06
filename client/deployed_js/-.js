a1114244939dabd1726d2909fd8358e352e5ae52
balraj-johal <balraj-johal@users.noreply.github.com>
2019-05-06 17:34:48 +0100

    css fixed

--- client/deployed_js/-.js
+++ client/deployed_js/-.js
@@ -1,19 +1,9 @@
 let exec = () => {
-quando.game.init_puzz('a23', ()=>{
-}, ()=>{
-quando.game.clear_puzzList()
-quando.text('no', false)
+quando.ar.whenMarker('B', 'flat', (markerID = 'B')=>{
+quando.ar.showGLTF(markerID, 'Avocado.gltf', 2500, false)
 })
-quando.addLabelStatement('1', ()=>{
-quando.game.change_puzzList('a')
+quando.ar.whenMarker('C', 'flat', (markerID = 'C')=>{
+quando.ar.showGLTF(markerID, 'tests/jupiter.gltf', 0.1, true)
 })
-quando.addLabelStatement('2', ()=>{
-quando.game.change_puzzList('2')
-})
-quando.addLabelStatement('3', ()=>{
-quando.game.change_puzzList('3')
-})
-quando.call_vis_rec('hand', ()=>{
-quando.text('YES', false)
-})
+quando.call_tts('humble as a mumble in the jungle ya diiiiig')
 }
