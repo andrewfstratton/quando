6524a3fa15d8915c9ac6cbd4c92692dc3e933187
andrewfstratton <A.Stratton@shef.ac.uk>
2019-05-27 10:43:17 +0100

    Tidy folders

--- utils/dump_db.js
+++
@@ -1,8 +0,0 @@
-const db = require('../db')
-
-db.dump('user', 'user_db.json').then((success)=>{
-    console.log(success)
-},(err)=>{console.log(err)})
-db.dump('script', 'script_db.json').then((success)=>{
-    console.log(success)
-},(err)=>{console.log(err)})
