6211b65815ad4cf57202485ebd80e731f2c87f5e
andrewfstratton <A.Stratton@shef.ac.uk>
2019-09-18 18:21:15 +0100

    Add mouse control Fixes #155, Fixes #156

--- client/modules/control.js
+++ client/modules/control.js
@@ -4,6 +4,7 @@
     alert('Fatal Error: Control must be included after quando_browser')
   }
   let self = quando.control = {}
+  let mouse = {}
 
   function _send(command, arg) {
     fetch('/control/' + command, { method: 'POST', 
@@ -32,4 +33,70 @@
     let val = {'key':ch, 'shift':shift, 'ctrl':ctrl, 'alt':alt, 'command':command}
     _send('key', val)
   }
+
+  self.mouseX = (x, smooth=false) => {
+    mouse.x = x
+    mouse.smooth = smooth
+  }
+
+  self.mouseY = (y, smooth=false) => {
+    mouse.y = y
+    mouse.smooth = smooth
+  }
+
+  self.mouseClick = (button, double=false) => {
+    let clicks = 1
+    if (double) {
+      clicks = 2
+    }
+    if (button == 'left') {
+      mouse.left = clicks
+    }
+    if (button == 'middle') {
+      mouse.middle = clicks
+    }
+    if (button == 'right') {
+      mouse.right = clicks
+    }
+  }
+
+  function _updateMouse() {
+    send = {}
+    if (mouse.hasOwnProperty('x')) {
+      send.x = mouse.x
+      send.valid = true
+      delete mouse.x
+    }
+    if (mouse.hasOwnProperty('y')) {
+      send.y = mouse.y
+      send.valid = true
+      delete mouse.y
+    }
+    if (mouse.hasOwnProperty('smooth')) {
+      send.smooth = mouse.smooth
+      send.valid = true
+      delete mouse.smooth
+    }
+    if (mouse.hasOwnProperty('left')) {
+      send.left = mouse.left
+      send.valid = true
+      delete mouse.left
+    }
+    if (mouse.hasOwnProperty('middle')) {
+      send.middle = mouse.middle
+      send.valid = true
+      delete mouse.middle
+    }
+    if (mouse.hasOwnProperty('right')) {
+      send.right = mouse.right
+      send.valid = true
+      delete mouse.right
+    }
+    if (send.valid) {
+      delete send.valid
+      _send('mouse', send)
+    }
+  }
+
+  setInterval(_updateMouse, 1/20 * 1000)
 })()
--- server/rest/control.js
+++ server/rest/control.js
@@ -31,4 +31,41 @@ module.exports = (app) => {
     robot.keyTap(val.key, modifier)
     res.json({})
   })
+
+  function _mouse_click(val, button) {
+    if (val.hasOwnProperty(button)) {
+      let double_click = false
+      if (val[button] == 2) {
+        double_click = true
+      }
+      robot.mouseClick(button, double_click)
+    }
+  }
+
+  app.post('/control/mouse', (req, res) => {
+    let val = req.body.val
+    let pos = robot.getMousePos()
+    let display = robot.getScreenSize()
+    let mouse = {x:pos.x, y:pos.y}
+    let smooth = false
+    robot.setMouseDelay(10)
+    if (val.hasOwnProperty('x')) {
+      mouse.x = val.x * display.width
+    }
+    if (val.hasOwnProperty('y')) {
+      mouse.y = (1-val.y) * display.height // Invert automatically...
+    }
+    if (val.hasOwnProperty('smooth')) {
+      smooth = val.smooth
+    }
+    if (smooth) {
+      robot.moveMouseSmooth(mouse.x, mouse.y)
+    } else {
+      robot.moveMouse(mouse.x, mouse.y)
+    }
+    _mouse_click(val, 'left')
+    _mouse_click(val, 'middle')
+    _mouse_click(val, 'right')
+    res.json({})
+  })
 }
