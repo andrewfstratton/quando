7ae49e4405f7027a92308c24a05e26451a7e432d
andrewfstratton <A.Stratton@shef.ac.uk>
2019-09-27 14:17:24 +0100

    Allow When Mouse movement to be destroyed Fixes #166 and tidy

--- client/modules/mouse.js
+++ client/modules/mouse.js
@@ -4,8 +4,8 @@
     alert('Fatal Error: mouse must be included after quando_browser')
   }
   let self = quando.mouse = {}
-  let mouse_move_x = []
-  let mouse_move_y = []
+  let mouse_move_x = {}
+  let mouse_move_y = {}
   let last_x = false
   let last_y = false
 
@@ -14,26 +14,27 @@
     if (event.buttons % 2) { // i.e. odd - which means left button held down
       button_down = true
     }
-    if (event.screenX != last_x) {
-      let val = event.screenX / window.screen.width
-      for (let handler in mouse_move_x) {
-        mouse_move_x[handler](val, button_down)
+    if (event.clientX != last_x) {
+      let val = event.clientX / window.innerWidth
+      for (let id in mouse_move_x) {
+        mouse_move_x[id](val, button_down)
       }
-      last_x = event.screenX
+      last_x = event.clientX
     }
-    if (event.screenY != last_y) {
-      let val = event.screenY / window.screen.height
-      for (let handler in mouse_move_y) {
-        mouse_move_y[handler](val, button_down)
+    if (event.clientY != last_y) {
+      // invert the mouse coordinates so up screen is positive
+      let val = 1 - (event.clientY / window.innerHeight)
+      for (let id in mouse_move_y) {
+        mouse_move_y[id](val, button_down)
       }
-      last_y = event.screenY
+      last_y = event.clientY
     }
   }
 
-  function _handleXY(button_down, push_to, middle, plus_minus, inverted, callback) {
+  function _handleXY(id, button_down, handlers, middle, plus_minus, inverted, callback) {
     let min = (middle - plus_minus)/100
     let max = (middle + plus_minus)/100
-    push_to.push((val, btn)=>{
+    handlers[id] = (val, btn)=>{
       if (btn == button_down) {
         if (inverted) { val = 1-val }
         if ((val >= min) && (val <= max)) {
@@ -41,16 +42,18 @@
           callback(v)
         }
       }
+    }
+    quando.destructor.add(() => {
+      delete handlers[id]
     })
   }
 
-  self.handleX = (button_down, middle, plus_minus, inverted, callback) => {
-    _handleXY(button_down, mouse_move_x, middle, plus_minus, inverted, callback)
+  self.handleX = (id, button_down, middle, plus_minus, inverted, callback) => {
+    _handleXY(id, button_down, mouse_move_x, middle, plus_minus, inverted, callback)
   }
 
-  self.handleY = (button_down, middle, plus_minus, inverted, callback) => {
-    _handleXY(button_down, mouse_move_y, middle, plus_minus, !inverted, callback)
-    // Note: deliberately invert mouse to get up screen positive?!
+  self.handleY = (id, button_down, middle, plus_minus, inverted, callback) => {
+    _handleXY(id, button_down, mouse_move_y, middle, plus_minus, inverted, callback)
   }
 
   addEventListener('mousemove', handle_mouse_xy)
