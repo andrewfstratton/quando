182910c97296dabd40469dacc0dc050c1c765183
andrewfstratton <A.Stratton@shef.ac.uk>
2020-06-02 22:40:05 +0100

    Move Java to 8080 port for running in parallel

--- local_server/src/uk/co/strattonenglish/quando/LocalServer.java
+++ local_server/src/uk/co/strattonenglish/quando/LocalServer.java
@@ -4,10 +4,12 @@ import org.eclipse.jetty.server.Server;
 
 public class LocalServer {
 
+	private static final int SERVER_PORT = 8080;
+
 	public static void main(final String[] args) throws Exception {
-		final int port = 80;
-		final Server server = new Server(port);
-        server.setHandler(new LocalhostHandler());
+		final Server server = new Server(SERVER_PORT);
+
+		server.setHandler(new LocalhostHandler());
 		server.start();
 		server.join();
 	}
