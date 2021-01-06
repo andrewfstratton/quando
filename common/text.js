04f53610a9aa8c4dc0483e3b26e1030b6f419c34
andrewfstratton <A.Stratton@shef.ac.uk>
2021-01-06 11:17:47 +0000

    Add common module for text encode/decode

---
+++ common/text.js
@@ -0,0 +1,17 @@
+import * as text from "/common/text.js";
+
+export function decode(str) {
+  return str.replace(/&amp;/g, '&')
+          .replace(/&lt;/g, '<')
+          .replace(/&gt;/g, '>')
+          .replace(/&quot;/g, '"')
+          .replace(/&apos;/g, "'")
+}
+
+export function encode(str) {
+  return str.replace(/&/g, '&amp;')
+          .replace(/</g, '&lt;')
+          .replace(/>/g, '&gt;')
+          .replace(/"/g, '&quot;')
+          .replace(/'/g, '&apos;')
+}
