e12ea34022235c43aa612be7d67ebf0b9b50c701
andrewfstratton <andrewfstratton@gmail.com>
2021-05-25 13:45:59 +0100

    Show local network IP instead of (not working) public IP

--- server/rest/ip.py
+++ server/rest/ip.py
@@ -5,6 +5,13 @@ import socket
 
 @app.route('/ip')
 def ip():
-    data = {"ip": socket.gethostbyname(socket.gethostname()),
-        "local":True, "success":True}
+    _ip ="127.0.0.1"
+    # See https://stackoverflow.com/questions/166506/finding-local-ip-addresses-using-pythons-stdlib
+    try:
+        _socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        _socket.connect(("10.255.255.255", 1))
+        _ip = _socket.getsockname()[0]
+    finally:
+        _socket.close()
+    data = {"ip": _ip, "local":True, "success":True}
     return jsonify(data)
