65c8ab419e71f76f4008d4cf7ff8edb66641f4a0
andrewfstratton <andrewfstratton@gmail.com>
2022-02-14 17:44:23 +0000

    Tidy handle keyboard

--- internal/server/devices/keyboard/keyboard.go
+++ internal/server/devices/keyboard/keyboard.go
@@ -3,7 +3,6 @@ package keyboard
 import (
 	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
 
 	"github.com/go-vgo/robotgo"
@@ -34,37 +33,22 @@ func type_string(str typeJSON) {
 	robotgo.TypeStr(string(str))
 }
 
-func getBytes(req *http.Request) []byte {
-	bytes, err := io.ReadAll(req.Body)
-	if err != nil {
-		fmt.Println("  read error ", err, string(bytes))
-		return nil
-	}
-	return bytes
-}
-
 func HandleKey(w http.ResponseWriter, req *http.Request) {
 	var key keyJSON
-	bytes := getBytes(req)
-	if bytes != nil {
-		err := json.Unmarshal(bytes, &key)
-		if err != nil {
-			fmt.Println("  unexpected parse error ", err, string(bytes))
-			return
-		}
-		press_release(key)
+	err := json.NewDecoder(req.Body).Decode(&key)
+	if err != nil {
+		fmt.Println("Error parsing request", err)
+		return
 	}
+	press_release(key)
 }
 
 func HandleType(w http.ResponseWriter, req *http.Request) {
 	var typeString typeJSON
-	bytes := getBytes(req)
-	if bytes != nil {
-		err := json.Unmarshal(bytes, &typeString)
-		if err != nil {
-			fmt.Println("  unexpected parse error ", err, string(bytes))
-			return
-		}
-		type_string(typeString)
+	err := json.NewDecoder(req.Body).Decode(&typeString)
+	if err != nil {
+		fmt.Println("Error parsing request", err)
+		return
 	}
+	type_string(typeString)
 }
