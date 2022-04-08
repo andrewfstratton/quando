4538592d004867fe3c78a64dc7d5ee321a772315
andrewfstratton <andrewfstratton@gmail.com>
2022-04-08 12:53:57 +0100

    Tidy local fields

--- internal/config/config.go
+++ internal/config/config.go
@@ -4,21 +4,21 @@ import (
 	"flag"
 )
 
-var _remote_client bool
-var _remote_editor bool
+var remote_client bool
+var remote_editor bool
 
 func RemoteClient() bool {
-	return _remote_client
+	return remote_client
 }
 
 func RemoteEditor() bool {
-	return _remote_editor
+	return remote_editor
 }
 
 func init() {
 	remote_client_p := flag.Bool("remote_client", false, "Allow client access from other machines")
 	remote_editor_p := flag.Bool("remote_editor", false, "Allow editor access from other machines - Warning - this has no security")
 	flag.Parse()
-	_remote_client = *remote_client_p
-	_remote_editor = *remote_editor_p
+	remote_client = *remote_client_p
+	remote_editor = *remote_editor_p
 }
