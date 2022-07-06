8e69171a87fe238d65a301d384172b8137723ee1
andrewfstratton <andrewfstratton@gmail.com>
2022-07-06 16:42:54 +0100

    Fix icon removed after quit or Ctrl-C

--- internal/tray/tray.go
+++ internal/tray/tray.go
@@ -4,9 +4,12 @@ package tray
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
+	"os/signal"
 	"quando/internal/icon"
 	"quando/internal/server"
+	"time"
 
 	"github.com/getlantern/systray"
 	"github.com/skratchdot/open-golang/open"
@@ -18,6 +21,12 @@ func Run() {
 }
 
 func setup() {
+	// handle OS interrupt
+	interrupt_channel := make(chan os.Signal)
+	signal.Notify(interrupt_channel, os.Interrupt)
+	go handleInterrupt(interrupt_channel)
+
+	// setup menu
 	systray.SetIcon(icon.Data())
 	systray.SetTitle("Quando")
 	systray.SetTooltip("Quando - noCode Toolset")
@@ -35,7 +44,6 @@ func setup() {
 			select {
 			case <-sysQuit.ClickedCh:
 				systray.Quit()
-				server.Quit()
 			case <-sysEditor.ClickedCh:
 				openChrome("/editor")
 			case <-sysClient.ClickedCh:
@@ -49,6 +57,12 @@ func setup() {
 	}()
 }
 
+func handleInterrupt(interrupt chan os.Signal) {
+	<-interrupt
+	fmt.Println("<<Interrupt>>")
+	systray.Quit()
+}
+
 func openChrome(suffix string) {
 	loc := lorca.LocateChrome()
 	cmd := exec.Command(loc, "--new-window", "--user-data-dir=C:\\chrome_dir", "--allow-insecure-localhost", "http://127.0.0.1"+server.Port()+suffix)
@@ -59,5 +73,7 @@ func openChrome(suffix string) {
 }
 
 func close() {
-	fmt.Println("Exiting...")
+	// fmt.Println("Close systray...")
+	time.Sleep(1 * time.Second)
+	server.Quit()
 }
