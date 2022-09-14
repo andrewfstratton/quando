da827e49a851e7d8f182857aef8cee190b20a819
andrewfstratton <andrewfstratton@gmail.com>
2022-09-14 16:05:39 +0100

    Uopdate leap to be local and kiosk for win 11

--- kiosk.bat
+++ kiosk.bat
@@ -1,3 +1,4 @@
-"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --kiosk "127.0.0.1/client" --user-data-dir="c:\quando\chrome_user"
-# Note: user-data-dir allows Chrome to be used separately from Quando.
-# Note: use Alt-F4 to exit the browser when in kiosk mode
+start "program" quando.exe
+start "program" "c:\program files\Google\Chrome\Application\chrome.exe" --kiosk "127.0.0.1/client" --user-data-dir="c:\chrome_dir"
+:: Note: user-data-dir allows Chrome to be used separately from Quando.
+:: Note: use Alt-F4 to exit the browser when in kiosk mode
