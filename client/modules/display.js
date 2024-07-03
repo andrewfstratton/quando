f8931562691581c1f1f3284d77c02d662739a22a
andrewfstratton <andrewfstratton@gmail.com>
2024-07-03 13:27:33 +0100

    Add weights for buttons/spacer/row and column Fix #505

--- client/modules/display.js
+++ client/modules/display.js
@@ -15,13 +15,21 @@ let buttons_parent = null // this is where to drop in new rows/columns
     return buttons_parent
   }
 
-  self.addButton = function({text = "", up_down = "down", text_colour, button_colour},fn) {
+  // Use an empty div for flew grow
+  function _getWeightedButton(weight, button) {
+    let div = document.createElement('div')
+    div.className = "quando_button_parent"
+    div.style.flexGrow = weight/100
+    div.appendChild(button)
+    return div
+  }
+
+  self.addButton = function({text = "", up_down = "down", weight = 100, text_colour, button_colour},fn) {
     let button = document.createElement('div')
     button.className = 'quando_button'
     button.innerHTML = text
     button.style.color = text_colour
     button.style.background = button_colour   
-    // set button.flexGrow to float weight
     button.setAttribute('id', 'button'+text)
     function handleTouch(ev) {
       ev.preventDefault() // Avoids double event
@@ -53,18 +61,19 @@ let buttons_parent = null // this is where to drop in new rows/columns
     button.ontouchend = handleTouch
     // button.onmousemove = handleTouch
     // button.ontouchmove = handleTouch
-    _getButtonsParent().appendChild(button)
+    _getButtonsParent().appendChild(_getWeightedButton(weight, button))
   }
 
-  self.addSpacer = function() {
+  self.addSpacer = function({weight = 100}) {
     let spacer = document.createElement('div')
     spacer.className = 'quando_button'
     spacer.style.opacity = 0
-    _getButtonsParent().appendChild(spacer)
+    _getButtonsParent().appendChild(_getWeightedButton(weight, spacer))
   }
 
-  self.buttonRow = function({row_flow=true}, fn) {
+  self.buttonRow = function({row_flow=true, weight=100}, fn) {
     let new_parent = document.createElement('div')
+    new_parent.style.flexGrow = weight/100
     if (row_flow) {
       new_parent.className = "quando_buttons_row"
     } else {
