86aea23a2248469632941603406d7cc09361fbb6
andrewfstratton <andrewfstratton@gmail.com>
2025-02-06 10:26:21 +0000

    initial xinput button check

--- internal/server/devices/gamepad/gamepad.go
+++ internal/server/devices/gamepad/gamepad.go
@@ -4,68 +4,70 @@ package gamepad
 
 import (
 	"fmt"
+	"syscall"
 	"time"
-
-	"github.com/0xcafed00d/joystick"
+	"unsafe"
 )
 
-const MAX_GAMEPAD = 8
+const (
+	NUM_GAMEPADS        = 4
+	XINPUT_DLL_FILENAME = "xinput1_3.dll"
+	XINPUT_GET_STATE    = "XInputGetState"
+)
 
+var getState *syscall.Proc
+
+type BUTTON_MASK uint16
 type Gamepad struct {
-	last_buttons uint32
-	last_axes    []int
-	joystick     joystick.Joystick
+	_             uint32 // packet - updates too often and not useful
+	button_masks  BUTTON_MASK
+	left_trigger  uint8
+	right_trigger uint8
+	left_x        int16
+	left_y        int16
+	right_x       int16
+	right_y       int16
 }
 
-var gamepads [MAX_GAMEPAD]Gamepad
+var gamepads [NUM_GAMEPADS]Gamepad
+var last_buttons [NUM_GAMEPADS]BUTTON_MASK
 
-func gamepadUpdate(num int) {
+
+func gamepadUpdate(num uint) {
 	gamepad := gamepads[num]
-	if joystick := gamepad.joystick; joystick != nil {
-		state, err := joystick.Read()
-		if err == nil {
-			buttons := state.Buttons
-			if gamepad.last_buttons != buttons {
-				fmt.Println(state.Buttons)
-				gamepads[num].last_buttons = buttons
-			}
-		} else { // dropped
-			gamepads[num].last_buttons = 0
-			gamepads[num].joystick = nil // to avoid reading
-			fmt.Println(num, " Lost...")
+	result, _, _ := getState.Call(uintptr(num), uintptr(unsafe.Pointer(&gamepad)))
+	if result == 0 { // success
+		if last_buttons[num] != gamepad.button_masks {
+			last_buttons[num] = gamepad.button_masks
+			fmt.Println("changed ", num, gamepad.button_masks)
 		}
-	}
-}
-
-func checkNewGamepads() {
-	for {
-		for num, gamepad := range gamepads {
-			if gamepad.joystick == nil {
-				js, err := joystick.Open(num)
-				if err == nil {
-					_, err := js.Read()
-					if err == nil {
-						gamepads[num].joystick = js
-						gamepads[num].last_buttons = 0
-						fmt.Println("Opened gamepad ", num)
-						fmt.Println("  Name: ", js.Name())
-						fmt.Println("  # buttons : ", js.ButtonCount())
-						fmt.Println("  # axes : ", js.AxisCount())
-					}
-				}
-			}
-			// else do nothing since we already have a handle
-		}
-		time.Sleep(time.Second / 2) // check 2 times a second for new joysticks
+	} else if last_buttons[num] != 0 {
+		last_buttons[num] = 0
+		fmt.Println("lost ", num)
 	}
 }
 
 func CheckChanged() {
-	go checkNewGamepads() // runs until application exits
+	if getState == nil {
+		fmt.Println("** XInput joystick not being checked...")
+		return
+	} // else
 	for {
-		// fmt.Print(".")
-		gamepadUpdate(0)
-		gamepadUpdate(1)
+		for gamepad := range NUM_GAMEPADS { // note this will be 0..3
+			gamepadUpdate(uint(gamepad))
+		}
 		time.Sleep(time.Second / 60) // 60 times a second
+	}
+}
+
+func init() {
+	dll, err := syscall.LoadDLL(XINPUT_DLL_FILENAME) // use older version for now
+	if err != nil {
+		fmt.Println("** Failed to find", XINPUT_DLL_FILENAME)
+	} else {
+		getState, err = dll.FindProc(XINPUT_GET_STATE)
+		if err != nil {
+			fmt.Println("** Failed to find proc :", XINPUT_GET_STATE)
+		}
 	}
 }
