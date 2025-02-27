7cdf7950c9984c2e0ed6043a10529420f2fb6eaa
andrewfstratton <andrewfstratton@gmail.com>
2025-02-27 11:12:44 +0000

    replace masks with button names

--- internal/server/devices/gamepad/gamepad.go
+++ internal/server/devices/gamepad/gamepad.go
@@ -30,26 +30,23 @@ type Gamepad struct {
 }
 
 var gamepads [MAX_GAMEPADS]*Gamepad // stores the last returned to identify changes - or nil
-var button_mask_index = [...]int{UP, DOWN, LEFT, RIGHT,
-	START, BACK, L_STICK, R_STICK, L_BUMPER, R_BUMPER,
-	A, B, X, Y}
 
 const (
-	UP       = 0x0001
-	DOWN     = 0x0002
-	LEFT     = 0x0004
-	RIGHT    = 0x0008
-	START    = 0x0010
-	BACK     = 0x0020
-	L_STICK  = 0x0040
-	R_STICK  = 0x0080
-	L_BUMPER = 0x0100
-	R_BUMPER = 0x0200
+	UP       = "UP"
+	DOWN     = "DOWN"
+	LEFT     = "LEFT"
+	RIGHT    = "RIGHT"
+	START    = "START"
+	BACK     = "BACK"
+	L_STICK  = "L_STICK"
+	R_STICK  = "R_STICK"
+	L_BUMPER = "L_BUMPER"
+	R_BUMPER = "R_BUMPER"
 	_        // guide not mapped
-	A        = 0x1000
-	B        = 0x2000
-	X        = 0x4000
-	Y        = 0x8000
+	A        = "A"
+	B        = "B"
+	X        = "X"
+	Y        = "Y"
 )
 
 func triggersChanged(gamepad_old, gamepad_new Gamepad) bool {
