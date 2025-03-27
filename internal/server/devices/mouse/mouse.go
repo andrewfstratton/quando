f5033c41d9eece0ff7ce0e930d679a1cc9020d27
andrewfstratton <andrewfstratton@gmail.com>
2025-03-27 17:22:36 +0000

    add initial relative with robotgo performance issue N.B. Scaled main display does not work

--- internal/server/devices/mouse/mouse.go
+++ internal/server/devices/mouse/mouse.go
@@ -6,6 +6,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/go-vgo/robotgo"
 )
@@ -46,21 +47,108 @@ func check_mouse(button_action string, last_action *string, button_name string) 
 	}
 }
 
+const INTERVAL = time.Duration(time.Second / 30)
+
+type movePair struct{ x, y *int }
+
+func newMovePair() movePair {
+	return movePair{&ZERO_MOVE, &ZERO_MOVE}
+}
+
+var ZERO_MOVE int = 0
+
+type moveChannel chan (movePair)
+
+var moves moveChannel
+
+func clamp(target *int, min, max int) {
+	if *target < min {
+		*target = min
+	} else if *target > max {
+		*target = max
+	}
+}
+
+var last_interval time.Time = time.Now()
+
+func interval(move movePair) {
+	// fmt.Print(".")
+	new_time := time.Now()
+	diff := new_time.Sub(last_interval)
+	last_interval = new_time
+	fmt.Println(diff)
+	if *move.x == 0 && *move.y == 0 { // i.e. no movement
+		return
+	} // else update location based on the interval and movement
+	current_x, current_y := robotgo.Location()
+	x := current_x + *move.x
+	y := current_y + *move.y
+	// apply limits
+	width, height := robotgo.GetScreenSize()
+	clamp(&x, 0, width)
+	clamp(&y, 0, height)
+	// fmt.Println("move:", x, y)
+	if current_x != x || current_y != y { // double check to avoid update when no movement
+		robotgo.Move(x, y) // update position : cannot use Smooth since that lifts the mouse buttons
+	}
+}
+
+func updateRelativeMove(moves moveChannel) {
+	move_pair := newMovePair()
+	for {
+		select {
+		case new_move := <-moves:
+			if new_move.x != nil {
+				move_pair.x = new_move.x
+			}
+			if new_move.y != nil {
+				move_pair.y = new_move.y
+			}
+			// fmt.Println("<-", *move_pair.x, *move_pair.y)
+		case <-time.After(INTERVAL):
+			interval(move_pair)
+		}
+	}
+}
+
+func intervalPixels(val float32, max_pixels int, time float32) int {
+	pixels := int((float32(max_pixels) * float32(INTERVAL.Seconds()) * (2 * (val - 0.5))) / time)
+	return pixels
+}
+
+func mouseMoveRelative(x_val, y_val *float32, time float32) {
+	width, height := robotgo.GetScreenSize()
+	move_pair := movePair{nil, nil}
+	if x_val != nil {
+		pixels := intervalPixels(*x_val, width, time)
+		move_pair.x = &pixels
+	}
+	if y_val != nil {
+		pixels := intervalPixels(*y_val, height, time)
+		move_pair.y = &pixels
+	}
+	moves <- move_pair
+}
+
 func move_press(mouse mouseJSON) {
 	check_mouse(mouse.Left, &last_left, "left")
 	check_mouse(mouse.Right, &last_right, "right")
 	check_mouse(mouse.Middle, &last_middle, "center")
 
-	if mouse.X != nil || mouse.Y != nil {
-		x, y := robotgo.GetMousePos()
-		width, height := robotgo.GetScreenSize()
-		if mouse.X != nil {
-			x = int(*mouse.X * float32(width))
+	if mouse.X != nil || mouse.Y != nil { // i.e. either x and/or y is changed
+		x, y := robotgo.Location()
+		if mouse.Relative {
+			mouseMoveRelative(mouse.X, mouse.Y, *mouse.Time)
+		} else { // i.e. absolute
+			width, height := robotgo.GetScreenSize()
+			if mouse.X != nil {
+				x = int(*mouse.X * float32(width))
+			}
+			if mouse.Y != nil {
+				y = int(*mouse.Y * float32(height))
+			}
+			robotgo.Move(x, y, robotgo.GetMainId())
 		}
-		if mouse.Y != nil {
-			y = int(*mouse.Y * float32(height))
-		}
-		robotgo.Move(x, y)
 	}
 }
 
@@ -72,4 +160,9 @@ func HandleMouse(w http.ResponseWriter, req *http.Request) {
 		return
 	}
 	move_press(mouse)
+}
+
+func init() {
+	moves = make(moveChannel, 0)
+	go updateRelativeMove(moves)
 }
