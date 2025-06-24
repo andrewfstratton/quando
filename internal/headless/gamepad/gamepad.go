09c1cd84f7aa44618fa7bfdaa918af873f76dbe5
andrewfstratton <andrewfstratton@gmail.com>
2025-06-24 21:27:41 +0100

    tidy and include sorted menu items Fix #578, #579

--- internal/headless/gamepad/gamepad.go
+++ internal/headless/gamepad/gamepad.go
@@ -16,17 +16,25 @@ type Defn struct {
 	_            text.Text         `txt:"🕹️️️️️ When "`
 	Index        menuinput.MenuInt `0:"Ⓐ/✕" 1:"Ⓑ/◯" 2:"Ⓧ/☐" 3:"Ⓨ/🛆" 14:"🠈" 15:"🠊" 12:"🠉" 13:"🠋" 4:"👈 Bumper" 5:"👉 Bumper" 10:"📍👈" 11:"👉📍" 8:"Back 👈" 9:"👉 Start"`
 	_            text.Text         `txt:" button " iconify:"true"`
-	PressRelease menuinput.MenuInt `2:"⇕" 1:"Press" 0:"Release"`
+	PressRelease menuinput.MenuInt `2:"⇕" 1:"press" 0:"release"`
 	Box          boxinput.Box
 }
+
+const (
+	Release      = 0
+	Press        = 1
+	PressRelease = 2
+)
 
 func init() {
 	defn := &Defn{}
 	library.Block(defn).Op(
 		func(early param.Params) func(param.Params) {
 			index := defn.Index.Param(early)
+			press_release := defn.PressRelease.Param(early)
 			return func(late param.Params) {
 				index.Update(late)
+				press_release.Update(late)
 				fmt.Println("Button :", index.Val)
 			}
 		})
