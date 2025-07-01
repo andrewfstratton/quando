529d9e3be07bffa7d58290d01607a4c3947be8db
andrewfstratton <andrewfstratton@gmail.com>
2025-07-01 22:36:49 +0100

    move comment

--- internal/headless/headless.go
+++ internal/headless/headless.go
@@ -2,8 +2,9 @@ package headless
 
 import (
 	"fmt"
+	// below are side effects, i.e. call init()
 	_ "quando/internal/headless/gamepad"
-	_ "quando/internal/headless/keyboard" // this calls init()
+	_ "quando/internal/headless/keyboard"
 	_ "quando/internal/headless/system/log"
 	_ "quando/internal/headless/time/after"
 	_ "quando/internal/headless/time/every"
